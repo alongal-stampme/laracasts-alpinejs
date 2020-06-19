ef41d430bb8e95b9dcba78abd409632678188e31
Alon Gal <alon@stampme.com>
2020-06-19 20:14:32 +1000

    Let's Build a Memory Game

--- src/resources/views/welcome.blade.php
+++ src/resources/views/welcome.blade.php
@@ -9,17 +9,50 @@
     </head>
     <body>
         <div x-data="game()" class="px-10 flex items-center justify-center min-h-screen">
+            <h1 class="fixed top-0 right-0 p-10 font-bold text-3xl">
+                <span x-text="points"></span>
+                <span class="text-xs">pts</span>
+            </h1>
+
             <div class="flex-1 grid grid-cols-4 gap-10">
                 <template x-for="card in cards">
-                    <div :style="'background: ' + (card.flipped ? card.color : '#999')"
-                         class="h-32 cursor-pointer"
-                         @click="flipCard(card)"
-                    ></div>
+                    <div>
+                        <button x-show="! card.cleared"
+                                :style="'background: ' + (card.flipped ? card.color : '#999')"
+                                class="w-full h-32"
+                                @click="flipCard(card)"
+                        >
+
+                        </button>
+                    </div>
                 </template>
             </div>
         </div>
 
+        {{-- flash message --}}
+        <div x-data="{ show: false, message: 'Default message' }"
+             x-show.transition.opacity="show"
+             x-text="message"
+             @flash.window="
+                message = $event.detail.message;
+                show=true;
+                setTimeout(() => show = false, 1000)
+            "
+             class="fixed bottom-0 right-0 bg-green-500 text-white p-2 mb-4 mr-4 rounded"
+        >
+        </div>
+
         <script>
+            function pause(milliseconds = 1000) {
+                return new Promise(resolve => setTimeout(resolve, milliseconds))
+            }
+
+            function flash(message) {
+                window.dispatchEvent(new CustomEvent('flash', {
+                    detail: { message }
+                }))
+            }
+
             function game() {
                 return {
                     cards: [
@@ -37,14 +70,45 @@
                         return this.cards.filter(card => card.flipped)
                     },
 
-                    flipCard(card) {
+                    get clearedCards() {
+                        return this.cards.filter(card => card.cleared)
+                    },
+
+                    get points() {
+                        return this.clearedCards.length
+                    },
+
+                    get remainingCards() {
+                        return this.cards.filter(card => ! card.cleared)
+                    },
+
+                    async flipCard(card) {
+                        if (this.flippedCards.length === 2) {
+                            return
+                        }
+
                         card.flipped = ! card.flipped
 
                         if (this.flippedCards.length === 2) {
-                            if (this.flippedCards[0]['color'] === this.flippedCards[1]['color']) {
-                                alert('you have a match')
+                            if (this.hasMatch()) {
+                                flash('You found a match!')
+                                await pause()
+
+                                this.flippedCards.forEach(card => card.cleared = true)
+
+                                if (! this.remainingCards.length) {
+                                    alert('you won!')
+                                }
                             }
+
+                            await pause()
+
+                            this.flippedCards.forEach(card => card.flipped = false)
                         }
+                    },
+
+                    hasMatch() {
+                        return this.flippedCards[0]['color'] === this.flippedCards[1]['color']
                     }
                 }
             }
