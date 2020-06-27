fc43f5a6ec09dca30fab7cbc82afb0dd6d83ab8a
Alon Gal <alon@stampme.com>
2020-06-27 11:41:34 +1000

    How and When to Extract Component Logic

--- src/resources/views/lesson.blade.php
+++ src/resources/views/lesson.blade.php
@@ -6,6 +6,7 @@
     <title>Laravel</title>
     <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.3.5/dist/alpine.min.js" defer></script>
+    <script src="/js/app.js"></script>
 </head>
 
 <body class="p-10 max-w-lg mx-auto">
@@ -51,5 +52,27 @@
         </template>
     </form>
 
+    <hr class="mb-8 mt-8">
+
+    <div x-data="taskApp()"
+        class="bg-gray-300 px-10 py-6 rounded">
+        <form @submit.prevent="submit()">
+            <input x-model="newTask"
+                   class="border border-gray-400 p-2 w-full"
+                   type="text"
+                   placeholder="Go to the market..."
+            >
+        </form>
+        <ul class="list-disc mt-3">
+            <template x-for="(task, index) in tasks" :key="index">
+                <li>
+                    <input type="checkbox" x-model="task.completed">
+                    <span x-text="task.body" :class="{ 'line-through' : task.completed }"></span>
+                </li>
+            </template>
+        </ul>
+    </div>
+
 </body>
+
 </html>
